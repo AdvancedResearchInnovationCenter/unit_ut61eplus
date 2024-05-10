6d725e9d83b54ce2acd9fc528df8137bc489da62
HussainMSajwani <hussainmsajwani@gmail.com>
2024-05-10 08:43:05 +0400

    changed to the mutlimeter we have at aric

--- readDMM.py
+++ readDMM.py
@@ -1,15 +1,26 @@
 
 import logging
 from ut61eplus import UT61EPLUS
-
+import time 
 log = logging.getLogger(__name__)
 
-logging.basicConfig(level=logging.DEBUG)
+# logging.basicConfig(level=logging.DEBUG)
 
 dmm = UT61EPLUS()
 
-log.info('name=%s', dmm.getName())
-dmm.sendCommand('lamp')
-m = dmm.takeMeasurement()
-log.info('measurent=%s', m)
 
+t = 0
+n = 0
+now = time.time()
+while True:
+    # log.info('name=%s', dmm.getName())
+    # dmm.sendCommand('lamp')
+    # dmm.takeMeasurement()
+    m = dmm.takeMeasurement()
+    n += 1
+    t += time.time() - now
+    now = time.time()
+    if n % 10 == 0:
+        print('n=%d, t=%f, t/n=%f' % (n, t, t/n))
+    # log.info('measurent=%s', m)
+    # print(m.display_decimal, m.display_unit, m.mode)
